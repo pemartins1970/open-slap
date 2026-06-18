b0e765be26edf2a721aa16d61989dcf09693871a
pemar <pemar@openslap.dev>
2026-06-18 16:44:40 -0300

    chore: remove 20 dead re-exports from hooks barrel (keep Grupo A files on disk)

--- frontend/src/hooks/index.js
+++ frontend/src/hooks/index.js
@@ -8,28 +8,10 @@ export { default as useChatSocket } from './useChatSocket';
 export { default as useChunkBuffer } from './useChunkBuffer';
 
 // Hooks novos - Utilitários
-export { useLocalStorage, useMultipleLocalStorage, useLocalStorageObject } from './useLocalStorage';
-export {
-  useDebounce,
-  usePrevious,
-  useFirstRender,
-  useMount,
-  useUnmount,
-  useToggle,
-  useCounter,
-  useArray,
-  useTimeout,
-  useInterval,
-  useClickOutside,
-  useCopyToClipboard,
-  useWindowSize,
-  useScrollPosition
-} from './useReactUtils';
-export { useModals, useModalWithData, useAppModals } from './useModals';
+export { useLocalStorage } from './useLocalStorage';
 
 // Hooks novos - Funcionalidades
 export { useConversations } from './useConversations';
-export { useMessages } from './useMessages';
 export { useSettings } from './useSettings';
 export { useLLMConfig } from './useLLMConfig';
 export { useSoul } from './useSoul';
